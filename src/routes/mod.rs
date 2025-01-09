d92d4d0cb052fc5f4c743a6a170f992b2a3bab90
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-01-10 00:18:36 +0500

    [newsletter] dummy publish newsletters

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,12 @@
 pub mod health_check;
+pub mod newsletters;
 pub mod subscriptions;
 pub mod subscriptions_confirm;
 
 pub use health_check::*;
+pub use newsletters::*;
 pub use subscriptions::*;
+pub use subscriptions_confirm::*;
 
 pub fn error_chain_fmt(
     e: &impl std::error::Error,
