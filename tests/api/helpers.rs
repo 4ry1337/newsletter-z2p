c88707809417bbb6cab54d1d56ca99196e066507
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-01-13 06:01:53 +0500

    [newsletter] blocking thread + securing from timing attacks
    [newsletter] sqlx offline
    
    [newsletter] clippy
    
    a

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,6 +1,6 @@
 use std::sync::LazyLock;
 
-use argon2::{password_hash::SaltString, Argon2, PasswordHasher};
+use argon2::{password_hash::SaltString, Algorithm, Argon2, Params, PasswordHasher, Version};
 use newsletter::{
     configuration::get_configuration,
     startup::Application,
@@ -160,10 +160,15 @@ impl TestUser {
 
     async fn store(&self, pool: &PgPool) {
         let salt = SaltString::generate(&mut rand::thread_rng());
-        let password_hash = Argon2::default()
-            .hash_password(self.password.as_bytes(), &salt)
-            .unwrap()
-            .to_string();
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).unwrap(),
+        )
+        .hash_password(self.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
+
         sqlx::query!(
             "INSERT INTO USERS (user_id, username, password_hash) VALUES ($1, $2, $3)",
             self.user_id,
--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,4 +1,5 @@
 use sqlx::PgPool;
+use uuid::Uuid;
 use wiremock::{
     matchers::{any, method, path},
     Mock, ResponseTemplate,
@@ -137,6 +138,63 @@ async fn requests_missing_authorization_are_rejected(pool: PgPool) {
     let response = reqwest::Client::new()
         .post(&format!("{}/newsletters", &app.address))
         .json(&newsletter_request_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(response.status().as_u16(), 401);
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
+}
+
+#[sqlx::test]
+async fn non_existing_user_is_rejected(pool: PgPool) {
+    let app = spawn_app(pool).await;
+    let username = Uuid::new_v4().to_string();
+    let password = Uuid::new_v4().to_string();
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!({
+            "title":"Newsletter Title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>"
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(response.status().as_u16(), 401);
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
+}
+
+#[sqlx::test]
+async fn invalid_password_is_rejected(pool: PgPool) {
+    let app = spawn_app(pool).await;
+
+    let username = &app.test_user.username;
+    let password = Uuid::new_v4().to_string();
+
+    assert_ne!(app.test_user.password, password);
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!({
+            "title":"Newsletter Title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>"
+            }
+        }))
         .send()
         .await
         .expect("Failed to execute request.");
